2937e7533cf87af26b338b4a599c0acd5e0c747f
Eliezer López <contacto@eliezerlopez.com>
2021-02-21 19:02:14 +0100

    La instrucción goto en C++

---
+++ ES_2583_01_05.cpp
@@ -0,0 +1,17 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+goto salto;
+    cout << "Texto de prueba 1" << endl;
+salto:
+    cout << "Texto de prueba 2" << endl;
+    return 0;
+}

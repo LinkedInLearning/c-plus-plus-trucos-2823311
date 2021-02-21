42bf7136a92386e016d962e5898ce2b1be1571c5
Eliezer López <contacto@eliezerlopez.com>
2021-02-21 19:40:32 +0100

    Métodos modificaciones en C++

--- ES_2583_02_03.cpp
+++ ES_2583_02_03.cpp
@@ -6,7 +6,20 @@
 
 #include <iostream>
 
+class Empleado {
+public:
+    bool setEdad(int e);
+private:
+    int edad;
+    float sueldo;
+};
+
 int main(){
 
     return 0;
+}
+
+bool Empleado::setEdad(int e){
+    edad = e;
+    return (edad==e);
 }

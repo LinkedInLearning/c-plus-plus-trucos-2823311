3f8bf797e4ab447537b6e28a3baa36632b22ea80
Eliezer López <contacto@eliezerlopez.com>
2021-02-21 21:06:35 +0100

    Diferenciando funciones y métodos por su signatura en C++

---
+++ ES_2583_02_06.cpp
@@ -0,0 +1,28 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool menor(int a, int b){
+    return (a<b);
+}
+
+bool menor(float a, float b){
+    return (a<b);
+}
+
+bool menor(string a, string b){
+    return (a<b);
+}
+
+int main(){
+
+    menor(4.2,5);
+
+    return 0;
+}

5fe7e558ac6d4ab0adb26cd98befd9a816c3a338
Eliezer López <contacto@eliezerlopez.com>
2021-02-21 21:12:21 +0100

    Paso de argumentos a la función main en C++

---
+++ ES_2583_02_07.cpp
@@ -0,0 +1,17 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * argv[]){
+
+    for(int i=0;i<argc;i++){
+        cout << "Argumento: " << i << " = " << argv[i] << endl;
+    }
+
+    return 0;
+}

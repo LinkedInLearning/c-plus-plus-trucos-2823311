09d266356b334b46c44c4474dbab5c21a12034e7
Eliezer López <contacto@eliezerlopez.com>
2021-02-21 19:07:57 +0100

    El tipo size_t en C++

---
+++ ES_2583_01_06.cpp
@@ -0,0 +1,18 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    for(size_t i=0; i<vector.lenght; i++){
+        cout << vector[i] << endl;
+    }
+
+    return 0;
+}

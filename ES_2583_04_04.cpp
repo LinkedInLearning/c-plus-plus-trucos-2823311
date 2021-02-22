78b19d92f36324aabe70335e026a8caf93e54364
Eliezer López <contacto@eliezerlopez.com>
2021-02-22 05:21:17 +0100

    Retorno constante en funciones en C++

---
+++ ES_2583_04_04.cpp
@@ -0,0 +1,18 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+
+const float& Empleado::calculoDeNomina(){
+    resultado = (sueldoMensualBruto - impuestos);
+    return resultado;
+}
+
+int main(){
+
+    
+    return 0;
+}

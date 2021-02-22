dea994c8304c97a54a666dbafbb17e7ec369cf74
Eliezer López <contacto@eliezerlopez.com>
2021-02-22 04:34:09 +0100

    Parámetros de entrada constantes en C++

---
+++ ES_2583_04_03.cpp
@@ -0,0 +1,19 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+
+float divide(const float numerador, float denominador){
+    if (denominador == 0) denominador = 1;
+    return (numerador / denominador);
+}
+
+int main(){
+
+
+
+    return 0;
+}

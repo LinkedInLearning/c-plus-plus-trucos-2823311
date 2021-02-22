10fee029021ebe58676856a604467077509980dc
Eliezer López <contacto@eliezerlopez.com>
2021-02-22 04:18:55 +0100

    Operador secuencia en C++

---
+++ ES_2583_03_04.cpp
@@ -0,0 +1,16 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+
+int main(){
+
+    int variable1, variable2;
+
+    for(int i=1, j=2; i+j < 20; i=i+1,j--);
+
+    return 0;
+}

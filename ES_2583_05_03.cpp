663d824f8a086818775a0f475b1bc7ab10e56998
Eliezer López <contacto@eliezerlopez.com>
2021-02-22 04:51:10 +0100

    Uso de la palabra reservada typedef en C++

---
+++ ES_2583_05_03.cpp
@@ -0,0 +1,16 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+
+int main(){
+
+    typedef unsigned int entero_positivo;
+
+    entero_positivo numero1;
+
+    return 0;
+}

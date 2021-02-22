df08292fbefebfdfa385c7b3f89fbf043e9c43d0
Eliezer López <contacto@eliezerlopez.com>
2021-02-22 04:27:02 +0100

    Variables constantes en C++

---
+++ ES_2583_04_01.cpp
@@ -0,0 +1,15 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+#define IVA 21
+
+int main(){
+
+    const float sueldo = 2500.00;
+    
+    return 0;
+}

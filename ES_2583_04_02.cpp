cb89674ccd42c48bd413ec566a2af8e6d9303599
Eliezer López <contacto@eliezerlopez.com>
2021-02-22 04:31:37 +0100

    Punteros constantes en C++

---
+++ ES_2583_04_02.cpp
@@ -0,0 +1,15 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+
+int main(){
+
+    const float sueldo;
+    const float *p_sueldo = &sueldo;
+
+    return 0;
+}

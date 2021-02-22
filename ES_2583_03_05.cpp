7adc0f7ff41f8b90dea2da7fbbff6e40a7e97571
Eliezer López <contacto@eliezerlopez.com>
2021-02-22 04:21:23 +0100

    Operador de modelado en C++

---
+++ ES_2583_03_05.cpp
@@ -0,0 +1,15 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+
+int main(){
+
+    int numerador = 6, denominador = 4;
+    int resultado = (float)numerador / (float)denominador;
+
+    return 0;
+}

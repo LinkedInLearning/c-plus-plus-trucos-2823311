57e113fde83361871e72a41b20d0187d4d9d009d
Eliezer López <contacto@eliezerlopez.com>
2021-02-22 04:06:02 +0100

    Uso de operadores con paréntesis en C++

---
+++ ES_2583_03_02.cpp
@@ -0,0 +1,20 @@
+/*
+ * Curso: C++ Trucos
+ * Plataforma: LinkedIn Learning
+ * Instructor: @EliezerLopez
+ * */
+
+#include <iostream>
+using namespace std;
+
+// https://es.cppreference.com/w/cpp/language/operator_precedence
+
+int main(){
+
+    int a = 80, b=3, c=6;
+    bool resultado = 4-a*8 || b / 4 != 9 && c % 4 == 3 || !false || a >= b;
+
+    cout << resultado << endl;
+
+    return 0;
+}
